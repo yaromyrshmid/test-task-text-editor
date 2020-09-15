032c24dac41620d0cbbef4c9be4986ff6cb50f94
yaromyr-shmid-ukrinsoft <yshmid@ukrinsoft.net>
2020-09-16 00:51:26 +0300

    Updates

---
+++ precache-manifest.6dc174848799f8ee9cbbdae2bb243b68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "501ca16384846273c116d13c3febe0cc",
+    "url": "/test-task-text-editor/index.html"
+  },
+  {
+    "revision": "6149bb0a754204b3d1c7",
+    "url": "/test-task-text-editor/static/css/main.075c6c49.chunk.css"
+  },
+  {
+    "revision": "e74e5c1c9d3656cf8dfa",
+    "url": "/test-task-text-editor/static/js/2.67605ba5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test-task-text-editor/static/js/2.67605ba5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6149bb0a754204b3d1c7",
+    "url": "/test-task-text-editor/static/js/main.a9e7233b.chunk.js"
+  },
+  {
+    "revision": "f73b03c9612dc5cb932b",
+    "url": "/test-task-text-editor/static/js/runtime-main.e1027218.js"
+  }
+]);
